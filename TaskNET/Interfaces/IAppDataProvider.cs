7aa0a74191244dcaa1aaa317445821b79cf6841a
dawidsobieszczuk <dawidsobieszczuk@gmail.com>
2025-09-04 07:53:48 +0200

    feat: create AppDataProvider interface

---
+++ TaskNET/Interfaces/IAppDataProvider.cs
@@ -0,0 +1,26 @@
+﻿using TaskNET.Models;
+
+namespace TaskNET.Interfaces
+{
+    public enum IncomingTasksFilter
+    {
+        Today,
+        Tomorrow,
+        ThisWeek
+    }
+
+    public interface IAppDataProvider
+    {
+        Task<IEnumerable<ToDoTask>> GetToDoTasksAsync();
+        Task<ToDoTask?> GetToDoTaskAsync(int id);
+        Task<IEnumerable<ToDoTask>> GetIncomingToDoTasksAsync(IncomingTasksFilter incomingTasksFilter = IncomingTasksFilter.Today);
+
+        Task<ToDoTask> CreateToDoTaskAsync(ToDoTask toDoTask);
+        Task<ToDoTask?> UpdateToDoTaskAsync(ToDoTask toDoTask);
+
+        Task<ToDoTask?> SetToDoTaskProgressAsync(int id, decimal percent);
+        Task<ToDoTask?> MarkToDoTaskAsDoneAsync(int id);
+        Task<bool> DeleteToDoTaskAsync(int id);
+
+    }
+}
