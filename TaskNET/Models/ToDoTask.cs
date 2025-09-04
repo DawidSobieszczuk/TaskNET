4bf976829d1eaade2f65923ae4a428a1b86f5cf2
dawidsobieszczuk <dawidsobieszczuk@gmail.com>
2025-09-04 07:52:40 +0200

    feat: create ToDoTask model

---
+++ TaskNET/Models/ToDoTask.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using TaskNET.Interfaces;
+
+namespace TaskNET.Models
+{
+    public class ToDoTask : IAuditable
+    {
+        public required int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+
+        public DateTime? ExpiryDate { get; set; }
+        [Column(TypeName = "decimal(5, 4)")]
+        public decimal PercentComplete { get; set; } = 0.0000M;
+        public bool IsDone { get; set; } = false;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
