bb0336995e67d7bbd9797acb3d508e1276b9f02f
dawidsobieszczuk <dawidsobieszczuk@gmail.com>
2025-09-04 07:50:06 +0200

    feat: add Swagger and SwaggerUI to API for development environment

--- TaskNET/Program.cs
+++ TaskNET/Program.cs
@@ -2,9 +2,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Configure the HTTP request pipeline.
 
